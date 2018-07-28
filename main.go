cdd4d4253d8f9d7d5c00112775570eadb902d900
yuichiro12 <talcnrhyme@gmail.com>
2018-07-28 19:22:35 +0900

    add func

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+var Params map[string]interface{}
 
+func OnRequest(m map[string]interface{}) {
+	for k, v := range Params {
+		m[k] = v
+	}
+}
+
+func SetParams(key string, value interface{}) {
+	Params[key] = value
+}
+
+func OnResponse(m map[string]interface{}) {
+	for k, v := range m {
+		Params[k] = v
+	}
+}
+
+func GetParams(key string) interface{} {
+	return Params[key]
 }
